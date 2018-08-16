56f8ee3eec877f3d6861a76c9742e6496366bc33
Danilo G. Baio <dbaio@bsd.com.br>
2018-08-16 20:53:23 -0300

    Add Jenkins script ci/copy_zanata_to_doc.sh

---
+++ ci/copy_zanata_to_doc.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#
+# Script to copy Zanata .po files into a FreeBSD doc repository 
+#
+
+if [ "$1" = "" ] || [ "$2" = "" ]; then
+	echo "Need to inform Zanata Directory and a FreeBSD doc repository"
+	echo "Example:"
+	echo
+	echo "$0 ./ ../freebsd-doc-br/pt_BR.ISO8859-1"
+	echo
+	exit 1
+fi
+
+if [ ! -z "$ZANATA_LOCALE" ]; then
+	ZANATA_LANG="$ZANATA_LOCALE"
+else
+	ZANATA_LANG="pt_BR"
+fi
+
+for article in $(find "$1/articles/$ZANATA_LANG" -name "*.po" ); do
+	fullname=$(basename "$article")
+	filename="${fullname%%.*}"
+	cp -vf "$article" "${2}/articles/${filename}/${ZANATA_LANG}.po"
+done
+
+for book in $(find "$1/books/$ZANATA_LANG" -name "*.po" ); do
+	fullname=$(basename "$book")
+	filename="${fullname%%.*}"
+	cp -vf "$book" "${2}/books/${filename}/${ZANATA_LANG}.po"
+done
+
